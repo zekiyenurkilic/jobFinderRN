bb8b3e07d3fedec4db794402532d797676255cca
zekiyenurkilic <70063407+zekiyenurkilic@users.noreply.github.com>
2021-01-04 10:33:53 +0300

    Update types.js

--- action/types.js
+++ action/types.js
@@ -1,5 +1,3 @@
-//export const baseUrl = 'https://secret-bastion-74542.herokuapp.com/api/v1';
-//export const baseUrl = 'http://192.168.1.34:3333/api/v1';
 
 export const GET_ALL_DATAS = 'GET_ALL_DATAS';
 export const SET_JOB_LIKE = 'SET_JOB_LIKE';
